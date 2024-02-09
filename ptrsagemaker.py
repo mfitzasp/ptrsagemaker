80c0fbc995117d89f39db9439ac222ca60a4cbd6
mfitzasp <mfitzasp@gmail.com>
2024-02-09 15:07:26 +1100

    Update ptrsagemaker.py

--- ptrsagemaker.py
+++ ptrsagemaker.py
@@ -23,12 +23,6 @@ except:
     os.system('pip install bokeh')
     import bokeh
 
-# try:
-#     #import pydrive
-#     from pydrive.auth import GoogleAuth , GoogleDrive
-# except:
-#     os.system('pip install pydrive')
-#     from pydrive.auth import GoogleAuth , GoogleDrive
 try:
     from PIL import Image, ImageFont, ImageDraw
 except:
@@ -107,8 +101,13 @@ except:
     os.system('pip install git+https://github.com/mfitzasp/ptrEXOTIC@main')
     import exotic
     from exotic.exotic import NASAExoplanetArchive, get_wcs, find_target
+    # Need to patch over the limb darkening tables as the default tries to link to an ftp server that blocks AWS for some reason
+    #wget.download('https://www.solarsiblings.com/ldtables/ldtk.py', out='~/.conda/envs/default/lib/python3.9/site-packages/ldtk/ldtk.py')
+    
 
-
+print ("********************")
+print ("PTRSAGEMAKER LOADED")
+print ("********************")
 
 def update_packages():
     os.system('pip install -U tqdm')
